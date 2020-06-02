1464c732498b8b0f79dae19668eccea017438f22
YushiChen <yushi.chen@skylinetw.com>
2020-06-02 16:51:35 +0800

    new version

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from handle_web import GetDailyMachine
+from new_handle_web import GetDailyMachine
 
 if __name__ == '__main__':
     machine = GetDailyMachine()
     machine.open_web()
-    machine.get_stocks_data()
+    done = False
+    while not done:
+        try:
+            machine.get_stocks_data()
+            done = True
+        except Exception as e:
+            print(f'error {e}')
