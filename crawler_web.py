8bd9394c11d361367f1b18b54d86427495fca7f7
YushiChen <yushi.chen@skylinetw.com>
2020-06-17 17:32:20 +0800

    all time

--- crawler_web.py
+++ crawler_web.py
@@ -86,6 +86,7 @@ class GetDailyMachine:
         if not self.show_web:
             opts.add_argument("--headless")
             opts.add_argument('--no-sandbox')
+            opts.add_argument('--disable-gpu')
         # opts.add_argument("--incognito")
         ua = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0"
         opts.add_argument("user-agent={}".format(ua))
--- main.py
+++ main.py
@@ -3,23 +3,27 @@ import time
 from crawler_web import GetDailyMachine
 
 if __name__ == '__main__':
-    machine = GetDailyMachine(show_web=False)
-    # print(machine.check_all_done())
-    # update stock_list.json
-    # machine.update_stocks()
+    while True:
+        machine = GetDailyMachine(show_web=False)
+        # print(machine.check_all_done())
+        # update stock_list.json
+        # machine.update_stocks()
 
-    # get data
-    machine.open_web()
+        # get data
+        machine.open_web()
 
-    done = False
-    while not done:
-        try:
-            machine.get_stocks_data()
-            print(machine.check_all_done())
-            time.sleep(5)
-            if machine.check_all_done():
-                done = True
-        except Exception as e:
-            machine.refresh_driver()
-            # machine.open_web()
-            print(f'error {e}')
+        done = False
+        while not done:
+            try:
+                machine.get_stocks_data()
+                print(machine.check_all_done())
+                time.sleep(5)
+                if machine.check_all_done():
+                    done = True
+            except Exception as e:
+                machine.refresh_driver()
+                # machine.open_web()
+                print(f'error {e}')
+        machine.close_web()
+        del machine
+        time.sleep(60 * 60 * 24)
