56963a71538083fdddaeb9f1957ee35006a34089
YushiChen <yushi.chen@skylinetw.com>
2020-06-19 18:50:28 +0800

    add check.py

--- check_download.py
+++ check_download.py
@@ -1,5 +1,7 @@
 if __name__ == '__main__':
     from crawler_web import GetDailyMachine
+    import os
 
     machine = GetDailyMachine(show_web=False)
-    machine.check_all_done()
+    check_set = set(machine.stocks) - set([i[:4] for i in os.listdir(machine.daily_path)] + list(machine.no_data))
+    print(check_set)
